fb911483fb255939ba324b3858af9bf899f90b80
Chen Haotian <chtunsw@gmail.com>
2019-01-16 15:57:35 +1100

    bootstrap added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
 "use strict";
+
+// onload event listener
+window.addEventListener('load',() => {
+    // hemepage init
+    document.getElementById('homepage').style.display = 'block';
+    // page switcher listener
+    pageLinkClickHandler();
+})
+
+// page link click event listener
+function pageLinkClickHandler() {
+    let navbar = document.getElementById('navbar')
+    navbar.addEventListener('click', (e) => linkClickSwitcher(e.target.id))
+}
+
+// page link click helper function
+function linkClickSwitcher(linkname) {
+    switch(linkname) {
+        case 'homelink':
+        pageDisplayControler('homepage')
+        break;
+        case 'resumelink':
+        pageDisplayControler('resumepage')
+        break;
+        case 'workslink':
+        pageDisplayControler('workspage')
+        break;
+        case 'commentslink':
+        pageDisplayControler('commentspage')
+        break;
+        default:
+    }
+}
+
+// page display controler
+function pageDisplayControler(activePageId) {
+    let pageIdList = ['homepage', 'resumepage', 'workspage', 'commentspage']
+    pageIdList.map(pageId => {
+        let displayValue = pageId === activePageId ? 'block' : 'none';
+        document.getElementById(pageId).style.display = displayValue;
+    })
+}
